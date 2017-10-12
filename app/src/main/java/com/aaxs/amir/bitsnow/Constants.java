db13eed81e7f61c3c6f50cba8808bb10289f75a7
Amir Ayub <dev.amirayub@gmail.com>
2017-10-13 01:39:28 +0400

    Pre FV Implementation

---
+++ app/src/main/java/com/aaxs/amir/bitsnow/Constants.java
@@ -0,0 +1,12 @@
+package com.aaxs.amir.bitsnow;
+
+/**
+ * Created by NitroV17 on 10/13/2017.
+ */
+
+public class Constants {
+
+    public static String URL ="";   //// TODO: 10/13/2017 ADD API ENDPOINT URL
+    public static boolean dev_mode =true;
+
+}

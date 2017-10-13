29cb037c4f812f979d0d5932d4467a4507e22955
Amir Ayub <dev.amirayub@gmail.com>
2017-10-13 05:19:03 +0400

    FV Implementation Phase3

--- app/src/main/java/com/aaxs/amir/bitsnow/MainActivity.java
+++ app/src/main/java/com/aaxs/amir/bitsnow/MainActivity.java
@@ -21,54 +21,56 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
-    private APIHandler apiHandler;
+    protected ArrayList<EventModel> arrayList = new ArrayList<>();
+    protected List<IFlexible> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        new APIHandler().execute();
-        apiHandler = new APIHandler();
-        List<IFlexible> myItems = apiHandler.list;
-        FlexibleAdapter<IFlexible> adapter = new FlexibleAdapter<>(myItems);
         recyclerView = (RecyclerView) findViewById(R.id.rv);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+
+        new APIHandler().execute();
+    }
+
+    public void init(){
+        List<IFlexible> myItems = list;
+        FlexibleAdapter<IFlexible> adapter = new FlexibleAdapter<>(myItems);
         recyclerView.setAdapter(adapter);
     }
 
-}
 
-class APIHandler extends AsyncTask<Void, Void, Void> {
+    class APIHandler extends AsyncTask<Void, Void, Void> {
 
-    private ArrayList<EventModel> arrayList = new ArrayList<>();
-    public List<IFlexible> list = new ArrayList<>();
-
-    @Override
-    protected Void doInBackground(Void... voids) {
-        HttpHandler sh = new HttpHandler();
-        String jsonStr = sh.makeServiceCall(apiEndpoint);
-        try {
-            if (jsonStr != null) {
-                JSONArray items = new JSONArray(jsonStr);
-                for (int i = 0; i < items.length(); i++) {
-                    JSONObject c = items.getJSONObject(i);
-                    String id = c.getString("id");
-                    String title = c.getString("name");    //title
-                    String desc = c.getString("title");      //desc
-                    arrayList.add(new EventModel(id, title, desc));
+        @Override
+        protected Void doInBackground(Void... voids) {
+            HttpHandler sh = new HttpHandler();
+            String jsonStr = sh.makeServiceCall(apiEndpoint);
+            try {
+                if (jsonStr != null) {
+                    JSONArray items = new JSONArray(jsonStr);
+                    for (int i = 0; i < items.length(); i++) {
+                        JSONObject c = items.getJSONObject(i);
+                        String id = c.getString("id");
+                        String title = c.getString("name");    //title
+                        String desc = c.getString("title");      //desc
+                        arrayList.add(new EventModel(id, title, desc));
+                    }
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        }catch (Exception e){
-            e.printStackTrace();
+            return null;
         }
-        return null;
-    }
-    @Override
-    protected void onPostExecute(Void result){
-        for(EventModel em : arrayList){
-            list.add(new EventAdapter(em.getId(),em.getTitle(),em.getDesc()));
+
+        @Override
+        protected void onPostExecute(Void result) {
+            for (EventModel em : arrayList) {
+                list.add(new EventAdapter(em.getId(), em.getTitle(), em.getDesc()));
+            }
+            init();
         }
     }
 }
